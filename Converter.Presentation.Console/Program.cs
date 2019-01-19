b6a46723cf3b14a47bff1c2568d704ab647ee10e
Darnley Costa <darnleycosta@gmail.com>
2019-01-19 12:35:33 -0200

    Added feature to write JSON file from console application

--- Converter.Presentation.Console/Program.cs
+++ Converter.Presentation.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Converter.ExcelConverter.Services;
+using Newtonsoft.Json;
 using System;
 using System.IO;
 
@@ -6,16 +7,40 @@ namespace Converter.Presentation.Console
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
+            // Test if input arguments were supplied:
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter the Microsoft Excel file path argument.");
+                System.Console.WriteLine("Usage: <path>");
+                return 1;
+            }
+
+            string excelFilePath = Path.GetFullPath(args[0]);
+
             ExcelConverterService service = new ExcelConverterService();
 
-            using (var stream = File.Open("D:/Users/Darnley/Desktop/file_excel.xlsx", FileMode.Open, FileAccess.Read))
+            using (var stream = File.Open(excelFilePath, FileMode.Open, FileAccess.Read))
             {
-                service.ConvertExcelToDictionary(stream);
+                var result = service.ConvertExcelToDictionary(stream);
+
+                string fileName = Path.GetFileNameWithoutExtension(excelFilePath);
+                string fileDirectory = Path.GetDirectoryName(excelFilePath);
+
+                string jsonFilePath = Path.Combine(fileDirectory, fileName + ".json");
+
+                using (StreamWriter file = File.CreateText(jsonFilePath))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    //serialize object directly into file stream
+                    serializer.Serialize(file, result);
+                }
             }
+
+            return 0;
         }
     }
 }
