bd4dc8c02ed738729b3192e622f5e95e7694e826
Darnley Costa <darnleycosta@gmail.com>
2019-01-21 20:21:32 -0200

    Added some comments to the default service

--- Converter.ExcelConverter/Services/ExcelConverterService.cs
+++ Converter.ExcelConverter/Services/ExcelConverterService.cs
@@ -7,6 +7,14 @@ namespace Converter.ExcelConverter.Services
 {
     public interface IExcelConverterService
     {
+        /// <summary>
+        /// Convert an Excel file in Stream to a Dictionary.
+        /// The inputted Excel file is supposed to be a data-only Excel, because we do not recognize graphs.
+        /// </summary>
+        /// <param name="fileStream">The Excel file in Stream</param>
+        /// <param name="useExcelTypes">Use the Excel cell's type. Otherwise, all will be string</param>
+        /// <param name="hasHeaders">Use the Excel first line as object name</param>
+        /// <returns>The Excel file converted to a standard Dictionary</returns>
         Dictionary<string, ICollection<Dictionary<string, object>>> ConvertExcelToDictionary(Stream fileStream, bool useExcelTypes = true, bool hasHeaders = true);
     }
 
@@ -17,6 +25,14 @@ namespace Converter.ExcelConverter.Services
             
         }
 
+        /// <summary>
+        /// Convert an Excel file in Stream to a Dictionary.
+        /// The inputted Excel file is supposed to be a data-only Excel, because we do not recognize graphs.
+        /// </summary>
+        /// <param name="fileStream">The Excel file in Stream</param>
+        /// <param name="useExcelTypes">Use the Excel cell's type. Otherwise, all will be string</param>
+        /// <param name="hasHeaders">Use the Excel first line as object name</param>
+        /// <returns>The Excel file converted to a standard Dictionary</returns>
         public Dictionary<string, ICollection<Dictionary<string, object>>> ConvertExcelToDictionary(Stream fileStream, bool useExcelTypes = true, bool hasHeaders = true)
         {
             using (var stream = fileStream)
